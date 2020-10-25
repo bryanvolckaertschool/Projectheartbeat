fd8116285d8aa8f2a84518c960af550c3cf9b554
MeFoxyy <melvin.devoss@gmail.com>
2020-10-25 16:40:04 +0100

    Melvin Vue Progress

--- Nodejs/client/src/views/Home.vue
+++ Nodejs/client/src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container fluid>
+      <v-row justify="space-around">
+        <v-col md=6 justify-self="center">
+          <v-card elevation="5" class="pa-8">
+            <v-card-title>Rusthuis</v-card-title>
+            <v-card-subtitle>Login of registreer uw rusthuis</v-card-subtitle>
+            <v-form>
+              <v-text-field
+                label="Naam"
+                v-model="Naam"
+                prepend-icon="person_pin"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                label="Email"
+                :type="'email'"
+                prepend-icon="mail"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                label="Wachtwoord"
+                :type="'password'"
+                prepend-icon="security"
+                required
+              ></v-text-field>
+
+              <v-container fluid>
+                <v-row justify="space-around">
+                  <v-btn class="px-10 mt-2 primary" depressed rounded>Login</v-btn>
+                  <v-btn class="px-10 mt-2" outlined rounded color="primary">Registreer</v-btn>
+                </v-row>
+
+              </v-container>
+      
+              
+            </v-form>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      Naam: "",
+      Email: "",
+      Wachtwoord: "",
+    };
+  },
+};
 </script>
